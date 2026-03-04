d5f88fcd1892d0a613237131e8b8367be00c8edc
artashesfrangyan <frangyan99@gmail.com>
2026-03-04 17:36:03 +0300

    Добавил предупреждение о необходимости перепроверки алиасов

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -2,13 +2,11 @@ import { Box, Container } from "@mui/material"
 import type { FC } from "react"
 import { Header } from "../Header/Header"
 import { Outlet } from "react-router-dom"
-import { TaskCard } from "../../../entities/task/ui/TaskCard"
 
 export const Layout: FC = () => {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100dvh' }}>
             <Header />
-            <TaskCard task={{ id: '2', status: 'created' }} />
             <Container component="main" sx={{ flex: 1, py: 3}}>
                 <Outlet />
             </Container>
