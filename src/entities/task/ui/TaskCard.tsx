48e8c0938345baf57a2573e4cff51115246c5de0
artashesfrangyan <frangyan99@gmail.com>
2026-03-04 17:33:45 +0300

    создал карточку задачи

---
+++ src/entities/task/ui/TaskCard.tsx
@@ -0,0 +1,33 @@
+import type { Task } from "../model/types"
+import type { FC } from "react"
+import { Box, Card, CardContent, Chip, Typography } from "@mui/material"
+
+const statusColors: Record<string, 'default' | 'warning' | 'success'> = {
+    created: 'default',
+    'in-progress': 'warning',
+    completed: 'success'
+}
+
+interface TaskCardProps {
+    task: Task;
+}
+
+export const TaskCard: FC<TaskCardProps> = ({ task }) => {
+    return (
+        <Card sx={{ mb: 2 }}>
+            <CardContent>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <Typography variant="body1">
+                        <strong>ID:</strong> {task.id}
+                    </Typography>
+                    <Chip
+                        label={task.status}
+                        color={statusColors[task.status]}
+                        size="small"
+                        sx={{ textTransform: 'capitalize' }}
+                    />
+                </Box>
+            </CardContent>
+        </Card>
+    )
+}
